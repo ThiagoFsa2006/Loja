ac73a2e130781518bc6862dfd3537f5c6d79da17
Thiago FSA <thiagofelipefsa@gmail.com>
2021-03-16 00:48:10 -0300

    New changes, trying to use table

--- testes.py
+++ testes.py
@@ -1,6 +1,9 @@
-from datetime import datetime
+import pandas as pd
 
+salario = 1
+carro = 2
 
-curr = datetime.now()
-ano = datetime.strftime(curr, "%Y")
-print(ano)
+data = [salario, carro],
+df = pd.DataFrame(data, columns=['salario', 'carros'])
+df.reset_index(index=False)
+print(df)
