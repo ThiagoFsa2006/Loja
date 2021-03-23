e1220d0aa1eec8396c3ba4ebd41d3e5390ce260c
Thiago FSA <thiagofelipefsa@gmail.com>
2021-03-23 20:36:38 -0300

    ATP 4 CONCLUIDA

--- loja.py
+++ loja.py
@@ -3,9 +3,9 @@ from datetime import datetime
 # import pandas as pd
 
 Loja = "Compre Tudo"
-Proprietario = "Thiago Felipe!"
+Proprietario = "Thiago"
 print("\n\nEsta é a loja", Loja, ", bem-vindo!\nAqui quem fala é o",
-      Proprietario, "\n")
+      Proprietario, "!\n")
 
 print(Fore.YELLOW + "Irei realizar sua análise de crédito.\n" + Fore.RESET)
 
@@ -15,6 +15,8 @@ salario = float(input("Digite seu salario: "))
 ano_nascimento = int(input("Digite seu ano de nascimento:"))
 ano_atual = datetime.strftime(curr, "%Y")
 idade = int(ano_atual) - int(ano_nascimento)
+fullname = len('Thiago Felipe Farias dos Santos')
+name = len(Proprietario)
 
 
 print("\nCargo: ", cargo)
@@ -27,26 +29,34 @@ print(Fore.YELLOW + 'Limite de crédito: R$ %.2f\n' %
       (limite_de_credito) + Fore.RESET)
 
 '''
+professor pode ignorar esta parte...
 d = {"Cargo": cargo, "salario": salario, "Ano de nascimento":
      ano_nascimento, "Idade": idade, "Limite de credito":   limite_de_credito}
 d = pd.Series(d)
 print(d)
 '''
-conda = (limite_de_credito * 0.6)
-condb = (limite_de_credito * 0.9)
+
+condA = (limite_de_credito * 0.6)
+condB = (limite_de_credito * 0.9)
 
 produto = str(input("Digite o nome do produto: "))
 preco_prod = float(input("Digite o preço do produto: "))
 
-if preco_prod <= conda:
-    print(Fore.GREEN + "\nAprovado!\n")
-    print(Fore.RESET)
-elif preco_prod < condb:
-    print(Fore.LIGHTYELLOW_EX + "\nLiberado para parcelar em 2 vezes!\n")
-    print(Fore.RESET)
+if preco_prod <= condA:
+    print(Fore.GREEN + "\nAprovado!\n" + Fore.RESET)
+elif preco_prod < condB:
+    print(Fore.LIGHTYELLOW_EX + "\nLiberado para parcelar em 2 vezes!\n" +
+          Fore.RESET)
 elif preco_prod < limite_de_credito:
-    print(Fore.YELLOW + "\nLiberado para parcelar em 3 ou mais vezes!\n")
-    print(Fore.RESET)
+    print(Fore.YELLOW + "\nLiberado para parcelar em 3 ou mais vezes!\n" +
+          Fore.RESET)
 else:
-    print(Fore.RED + "\nBloqueado!\n")
-    print(Fore.RESET)
+    print(Fore.RED + "\nBloqueado!\n" + Fore.RESET)
+
+if preco_prod < idade:
+    preco_prod = preco_prod - (preco_prod * (name / 100))
+    print("Desconto:", str(name), '%')
+    print("Valor do produto com desconto: R$ %.2f\n" % (preco_prod))
+else:
+    print("Produto:", str(produto))
+    print("Valor do produto: R$ %.2f\n" % preco_prod)
