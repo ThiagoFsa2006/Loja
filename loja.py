2fdeb2f96c59b6e1275cd25bcfdc26941a5f3042
Thiago FSA <thiagofelipefsa@gmail.com>
2021-03-22 22:19:07 -0300

    ET 4 1/2 COMPLETE

--- loja.py
+++ loja.py
@@ -1,6 +1,6 @@
 from colorama import Fore
 from datetime import datetime
-import pandas as pd
+# import pandas as pd
 
 Loja = "Compre Tudo"
 Proprietario = "Thiago Felipe!"
@@ -26,10 +26,27 @@ limite_de_credito = (salario * (idade / 1000)) + 100
 print(Fore.YELLOW + 'Limite de crédito: R$ %.2f\n' %
       (limite_de_credito) + Fore.RESET)
 
-
+'''
 d = {"Cargo": cargo, "salario": salario, "Ano de nascimento":
      ano_nascimento, "Idade": idade, "Limite de credito":   limite_de_credito}
-
 d = pd.Series(d)
-
 print(d)
+'''
+conda = (limite_de_credito * 0.6)
+condb = (limite_de_credito * 0.9)
+
+produto = str(input("Digite o nome do produto: "))
+preco_prod = float(input("Digite o preço do produto: "))
+
+if preco_prod <= conda:
+    print(Fore.GREEN + "\nAprovado!\n")
+    print(Fore.RESET)
+elif preco_prod < condb:
+    print(Fore.LIGHTYELLOW_EX + "\nLiberado para parcelar em 2 vezes!\n")
+    print(Fore.RESET)
+elif preco_prod < limite_de_credito:
+    print(Fore.YELLOW + "\nLiberado para parcelar em 3 ou mais vezes!\n")
+    print(Fore.RESET)
+else:
+    print(Fore.RED + "\nBloqueado!\n")
+    print(Fore.RESET)
