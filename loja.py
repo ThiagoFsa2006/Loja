aa6912ac3a937cbcd34f24db03cc740a6b03cfa0
Thiago FSA <thiagofelipefsa@gmail.com>
2021-04-12 22:39:38 -0300

    Completed

--- loja.py
+++ loja.py
@@ -28,54 +28,53 @@ def obter_limite():
     print('Idade:\t\t\t\t' + str(idade) + ' anos.\n')
     print(Fore.YELLOW + 'Limite de crédito:\t\tR$ %.2f\n' %
           (limite_de_credito) + Fore.RESET)
-    return limite_de_credito
+    qtde = int(input("Quantos itens pretende informar? "))
+
+    return limite_de_credito, idade, qtde
 
 
-limite = obter_limite()
+limite_de_credito, idade, qtde = obter_limite()
 
 # VERIFICAÇÃO SE O CLIENTE PODERA COMPRAR A VISTA OU,
 # SE DEVERA PARCELAR OU AINDA SE ESTA BLOQUADO PARA A COMPRA
 
 
-def verifica_produto(limite):
-
-    condA = (limite * 0.6)
-    condB = (limite * 0.9)
+def verifica_produto(limite_de_credito, cont):
+    condA = (limite_de_credito * 0.6)
+    condB = (limite_de_credito * 0.9)
     produto = str(input("Digite o nome do produto:\t"))
     preco_prod = float(input("Digite o preço do produto:\t"))
 
-    if preco_prod <= condA:
-        print(Fore.GREEN + "\nAprovado!\n" + Fore.RESET)
-    elif preco_prod < condB:
-        print(Fore.LIGHTYELLOW_EX + "\nLiberado para parcelar em 2 vezes!\n" +
-              Fore.RESET)
-    elif preco_prod < limite:
-        print(Fore.YELLOW + "\nLiberado para parcelar em 3 ou mais vezes!\n" +
-              Fore.RESET)
+    if preco_prod < limite_de_credito:
+        if preco_prod <= condA:
+            print(Fore.GREEN + "\nAprovado!\n" + Fore.RESET)
+        elif preco_prod < condB:
+            print(Fore.LIGHTYELLOW_EX +
+                  "\nLiberado para parcelar em 2 vezes!\n" + Fore.RESET)
+        elif preco_prod < limite_de_credito:
+            print(Fore.YELLOW +
+                  "\nLiberado para parcelar em 3 ou mais vezes!\n" +
+                  Fore.RESET)
+
+        if preco_prod > fullname and preco_prod < idade:
+            preco_prod = preco_prod - (preco_prod * (name / 100))
+            print("Desconto:\t\t\t" + str(name) + '%')
+            print("Valor do produto com desconto:\tR$ %.2f\n" % (preco_prod))
+        else:
+            print("Produto:\t\t\t" + str(produto).capitalize())
+            print("Valor do produto:\t\tR$%.2f\n" % preco_prod)
+            limite_de_credito = limite_de_credito - preco_prod
+            cont = cont + 1
     else:
-        print(Fore.RED + "\nBloqueado!\n" + Fore.RESET)
-
-    # VERIFICAÇÃO DE POSSIVEL DESCONTO DE VALOR DO PRODUTO ESTIVER ENTRE
-    # NUMERO DE CARACTERES DO NOME COMPLETO E IDADE
-    # NESTE CASO A VARIAVEL fullname TEM 31 CARACTERES DO ALUNO
-
-    if preco_prod > fullname and preco_prod < 50:
-        preco_prod = preco_prod - (preco_prod * (name / 100))
-        print("Desconto:\t\t\t" + str(name) + '%')
-        print("Valor do produto com desconto:\tR$ %.2f\n" % (preco_prod))
-    elif preco_prod > limite:
-        print("Não há limite disponivel para esta compra")
-    else:
-        print("Produto:\t\t\t" + str(produto).capitalize())
-        print("Valor do produto:\t\tR$%.2f\n" % preco_prod)
-    return preco_prod
+        print(Fore.RED + "\nBLOQUEADO!!!" + Fore.RESET)
+        print(Fore.RED + "\nNão existe limite de credito disponível para "
+              "este item")
+        print("Por favor, informe um item de menor valor\n" + Fore.RESET)
+    return limite_de_credito, cont
 
 
-qtde = int(input("Quantos itens pretende informar? "))
-
-i = 0
-while i < qtde:
-    limite = limite - verifica_produto(limite)
+cont = 0
+while cont < qtde:
+    limite_de_credito, cont = verifica_produto(limite_de_credito, cont)
     print(Fore.YELLOW + 'Limite disponivel para compras:\t\tR$ %.2f\n' %
-          (limite) + Fore.RESET)
-    i = i + 1
+          (limite_de_credito) + Fore.RESET)
